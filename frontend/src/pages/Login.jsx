ce028f83b2904e801f8776411c6f4af7ca0f1dc2
DavidCorreaV <david.correav@gmail.com>
2022-03-14 23:49:35 -0500

    Front pages and login forms

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log into your account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter your Email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Enter your Password"
+              id="password"
+              value={password}
+              onChange={onChange}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+};
+
+export default Login;
